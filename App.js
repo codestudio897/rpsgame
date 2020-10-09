b3730fa8de2a0d08a1f5645d4328d05ca15244c5
Chinmay Kulthe <chinmaykulthe897@gmail.com>
2020-10-09 10:40:32 +0530

    added web build

--- App.js
+++ App.js
@@ -1,14 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import PlayScreen from './src/PlayScreen'
+
+
 
 export default function App() {
+
+  const [play, setPlay] = useState(false)
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    play ? (<PlayScreen/>) : (
+    
+      <View style={styles.container}>
+           <Text style={styles.header}>Rock Paper Scissors</Text>
+        <Text style={{fontSize: 70}}>✊ ✋ ✌️</Text>
+        <View style={styles.button}>
+        <Button onPress={()=> setPlay(true)} title="Play"/>
+        </View>
+        <StatusBar style="auto" />
+      </View>
+    )
+    );
 }
 
 const styles = StyleSheet.create({
@@ -18,4 +31,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header: {
+    fontSize: 28,
+    marginBottom: 30
+  },
+  button: {
+    width: 240,
+    marginTop: 50,
+  }
 });
